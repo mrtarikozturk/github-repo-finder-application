9be3dc691975a922b001f18a0df3cfeba51e8bdc
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-06 23:57:34 +0200

    Add js files

---
+++ app.js
@@ -0,0 +1,22 @@
+//Elements
+const searchInput = document.querySelector('.searchGitHub');
+
+
+//EventListeners
+searchInput.addEventListener('keyup', get);
+
+function get(e) {
+    
+    const inputText = e.target.value;
+    
+    if(inputText !== ''){
+
+
+
+        
+    }
+    
+
+}
+    
+
