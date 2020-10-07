d8f337bd45c5839be8ef5026ef78ab03a77e6c3e
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-07 23:55:17 +0200

    Add getRepo and displayRepo functions

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //Elements
 const searchInput = document.querySelector('.searchGitHub');
 
+// inits
+const github =new Github();
+const ui = new UI();
+
 
 //EventListeners
 searchInput.addEventListener('keyup', get);
@@ -11,9 +15,17 @@ function get(e) {
     
     if(inputText !== ''){
 
+        // Make an HTTP request to GitHub API
+        github.getRepo(inputText).then(data => {
 
 
+        ui.displayRepo(data.repo);       
+
+        });
         
+
+    }else{
+
     }
     
 
